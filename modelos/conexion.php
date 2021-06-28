240e3e18e3f0220002f64bb254a0e291ea664251
Megacursos <58077785+Megacursos@users.noreply.github.com>
2021-06-28 15:56:59 -0500

    Update conexion.php

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,9 +4,9 @@ class Conexion{
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:https://databases.000webhost.com;dbname=id17149195_ventas",
-			            "id17149195_root",
-			            "Ou{>#?R-DXhS!e1F");
+		$link = new PDO("mysql:sql10.freesqldatabase.com;dbname=sql10421945",
+			            "sql10421945",
+			            "cwx66emECQ");
 
 		$link->exec("set names utf8");
 
