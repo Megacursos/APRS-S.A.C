9652b1f7041bc84b93588d32be049d4e2e6381b8
Megacursos <58077785+Megacursos@users.noreply.github.com>
2021-06-28 15:05:35 -0500

    Update conexion.php

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -5,8 +5,8 @@ class Conexion{
 	static public function conectar(){
 
 		$link = new PDO("mysql:https://databases.000webhost.com;dbname=id17149195_ventas",
-			            "id17149195_ventas2021",
-			            "msmWS)}0Xu*Orl+7");
+			            "id17149195_root",
+			            "Ou{>#?R-DXhS!e1F");
 
 		$link->exec("set names utf8");
 
